72edc4954376d75fd05770f6de9e0732825c03e4
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-11-29 04:58:39 +0500

    create two class take access of first class in second class

---
+++ Person.cs
@@ -0,0 +1,6 @@
+// using System;
+//   public class New{}
+
+// namespace Sheraz{ 
+
+// }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace First
+{
+    public class Person{
+        public void Greeting(){
+            System.Console.WriteLine("Tested  Person");
+        }
+    }
+
+
+    class Program
+    {
+ 
+        public static void Main(string[] args)
+        {   
+            Person person=new Person();
+            person.Greeting();
+            System.Console.WriteLine("Hello world");
+        }
+    }
+
+}
